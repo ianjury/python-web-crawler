6d017cab77e84611e0adfa371a966a06d25a6720
ianjury <mrianjury@gmail.com>
2017-10-28 17:12:27 -0700

    Fixed regex bugs

--- pr1.py
+++ pr1.py
@@ -11,24 +11,28 @@ import random
 
 def process_web_page(url, count, already_visited_pages, total_word_list):
     if count < 50:  # limits web page visiting to 50, as specified in requirements
-        page = urllib.request.urlopen(url)
-        page_text = str(page.read())
-        links = re.findall(r'a href[\s+]?=[\s+]?["]?([^"\s >]+)', page_text)  # gets absolute urls in current page
-        links = list(set(links))  # make links unique, just in case they are reference multiple times.
-        write_connections_to_csv(url, links)
+        print(url)
+        try:
+            page = urllib.request.urlopen(url)
+            page_text = str(page.read())
+            links = re.findall(r'a href[\s+]?=[\s+]?["]?(http[^"\s >]+)', page_text)  # gets absolute urls in current page
+            links = list(set(links))  # make links unique, just in case they are reference multiple times.
+            write_connections_to_csv(url, links)
 
-        total_word_list += re.findall('<[hp][1-9]?>(.*?)</[hp][1-9]?>', page_text)  # get text
-        #print(total_word_list)
+            total_word_list += re.findall('<[hp][1-9]?>(.*?)</[hp][1-9]?>', page_text)  # get text
+            #print(total_word_list)
 
-        for sub_url in links:  # for each url, visit the children urls
-            if sub_url not in already_visited_pages:
-                count += 1
-                already_visited_pages.append(sub_url)  # remember which pages have already been visited (prevent loops)
-                process_web_page(sub_url, count, already_visited_pages, total_word_list)  # recursively crawl through child pages
-                # print(sub_url, count)
+            for sub_url in links:  # for each url, visit the children urls
+                if sub_url not in already_visited_pages:
+                    count += 1
+                    already_visited_pages.append(sub_url)  # remember which pages have already been visited (prevent loops)
+                    process_web_page(sub_url, count, already_visited_pages, total_word_list)  # recursively crawl through child pages
+        except:
+            pass
         # print(alreadyVisitedPages)
     else:
         print('Exceeded 50 sites.')
+        return None
 
     return total_word_list
 
@@ -98,7 +102,7 @@ def create_word_cloud(word_dict):
 def main():
     open('results.csv', 'w').close()  # clear contents of csv from previous executions of the program
     test_files = ['urls2.txt', 'urls3.txt', 'urls6.txt']  # holds names of test files provided
-    url_list_file = open(test_files[0], 'r')  # change index here to go between test files.
+    url_list_file = open(test_files[2], 'r')  # change index here to go between test files.
     url_list = []
     for url in url_list_file:  # get list of urls in file
         url = url.rstrip()  # remove newline
